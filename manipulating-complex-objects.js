149f101cbb63c6950e3051e9ddb5b3a001cd6a57
Mariya Tumelya <mtumelia@gmail.com>
2022-06-16 09:53:53 +0300

    feat: add new solution manipulating-complex-objects

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,15 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": ["CD", "8T", "LP"],
+        "gold": true
+    },
+    {
+        "artist": "Arnold",
+        "title": "Villain",
+        "release_year": 1983,
+        "formats": ["CD", "8T", "LP"]
+    }
+];
